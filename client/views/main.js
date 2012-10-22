c63f0785947f20cb0bec73fbd79ee746254cf026
Per Nilsson <pnilsson@zynga.com>
2012-10-21 22:30:08 -0700

    Remove unused event handler.

--- client/views/main.js
+++ client/views/main.js
@@ -2,10 +2,3 @@ Template.main.date = function () {
     var d = new Date();
     return (d.getMonth() + 1) + '/' + d.getDate() + ' ' + d.getFullYear();
 };
-
-Template.main.events({
-    'click input[name="add_task"]' : function () {
-        // template data, if any, is available in 'this'
-        console.log('clickity');
-    }
-});
