9e8b5e26e7a0cfc6172cfb548cdc88e98ba9524d
Per Nilsson <pnilsson@zynga.com>
2012-10-25 20:01:44 -0700

    Add note on blur and return keypress.

--- client/views/add_task.js
+++ client/views/add_task.js
@@ -1,7 +1,15 @@
 var handleAddTask = function(e){
-    var $el = $(e.target),
-        text = $.trim($el.val());
-    if (text.length > 0) {
+
+    // Enter new task if return was pressed, or if this event is blur.
+    if (e.type === 'blur' || (e.type === 'keyup' && e.keyCode === 13)) {
+        var $el = $(e.target),
+            text = $.trim($el.val());
+
+        // Abort on empty string.
+        if (text.length === 0) {
+            return;
+        }
+
         Task.create(text);
         $el.val('');
         $el.parent().hide();
@@ -9,5 +17,6 @@ var handleAddTask = function(e){
 };
 
 Template.add_task.events({
-    'blur .add_task input': handleAddTask
+    'blur .add_task input': handleAddTask,
+    'keyup .add_task input': handleAddTask
 });
