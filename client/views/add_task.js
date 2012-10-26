3388f62c8c311eb840413062f451de614b64a147
Per Nilsson <pnilsson@zynga.com>
2012-10-25 19:53:07 -0700

    Do not create a new note with 0 length.

--- client/views/add_task.js
+++ client/views/add_task.js
@@ -1,9 +1,11 @@
 var handleAddTask = function(e){
     var $el = $(e.target),
-        text = $el.val();
-    Task.create(text);
-    $el.val('');
-    $el.parent().hide();
+        text = $.trim($el.val());
+    if (text.length > 0) {
+        Task.create(text);
+        $el.val('');
+        $el.parent().hide();
+    }
 };
 
 Template.add_task.events({
